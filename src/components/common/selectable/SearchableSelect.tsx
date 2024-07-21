904771de2d4d8a06c1c1d46c397034313fdf4ce6
Ashish Kumar <ashishkhello@gmail.com>
2024-07-21 17:32:43 +0530

    cross icon fixes

--- src/components/common/selectable/SearchableSelect.tsx
+++ src/components/common/selectable/SearchableSelect.tsx
@@ -1,8 +1,8 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { Select } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
-import { CrossIcon } from "lucide-react";
+import { X } from "lucide-react";
 
 const SearchableSelect = ({
   options,
@@ -25,6 +25,16 @@ const SearchableSelect = ({
     option.label.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  /*close menu handler*/
+  const closeMenu = useCallback((value: string, isValue = false) => {
+    if (isValue) {
+      setSelectedOption(value);
+      selectCallback(value);
+      setSearchTerm(label);
+    }
+    setIsMenuOpen(false);
+  }, []);
+
   return (
     <div className="relative">
       <div className="text-base font-semibold">{label}</div>
@@ -38,19 +48,13 @@ const SearchableSelect = ({
         placeholder={placeholder}
         className="mb-2 border border-gray-300 rounded p-2"
         onFocus={() => setIsMenuOpen(true)}
-        onBlur={() => setIsMenuOpen(false)}
       />
       {isMenuOpen && filteredOptions.length > 0 && (
         <div className="absolute z-10 w-full bg-white border border-gray-300 rounded shadow-md mt-1 max-h-60 overflow-auto">
           {filteredOptions.map(({ label, value }, idx) => (
             <div
               key={idx}
-              onClick={() => {
-                setSelectedOption(value);
-                selectCallback(value);
-                setIsMenuOpen(false);
-                setSearchTerm(label);
-              }}
+              onClick={() => closeMenu(value, true)}
               className="cursor-pointer p-2 hover:bg-gray-200"
             >
               {label}
@@ -58,17 +62,17 @@ const SearchableSelect = ({
           ))}
         </div>
       )}
-      {selectedOption && (
+      {true && (
         <div className="flex items-center justify-between p-2 mt-2 bg-gray-100 rounded border border-gray-300">
           <div>{selectedOption}</div>
           <button
             onClick={() => {
               setSelectedOption(null);
-              selectCallback(null);
+              selectCallback("");
             }}
             className="text-gray-500 hover:text-gray-700 focus:outline-none"
           >
-            <CrossIcon className="w-5 h-5" />
+            <X className="w-5 h-5" />
           </button>
         </div>
       )}
